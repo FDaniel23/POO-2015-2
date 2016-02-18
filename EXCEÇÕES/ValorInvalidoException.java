c276b0ad8f0e31add4444bfb0028272b3a850156
Felipe Daniel <f.danyel@gmail.com>
2016-02-17 21:32:45 -0300

    EX 11.11
    EXCEÇÕES

---
+++ EXCEÇÕES/ValorInvalidoException.java
@@ -0,0 +1,11 @@
+
+public class ValorInvalidoException extends RuntimeException {
+	
+	
+public ValorInvalidoException(double valor) {
+	
+	super("Valor invalido"+ valor);
+	// TODO Auto-generated constructor stub
+}
+
+}
